155a1e0d9d06ac54f1c11f1c868ea1363be4957f
PABLOESTRADA20 <pablo.maximiliano.cocio.estrada@gmail.com>
2026-03-21 01:09:56 -0300

    error arreglado

--- script.js
+++ script.js
@@ -24,26 +24,26 @@ mainBtn.addEventListener('click', () => {
 });
 
 const observerOptions = { threshold: 0.2 };
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.style.opacity = "1";
-                entry.target.style.transform = "translateY(0)";
-            }
-        });
-    }, observerOptions);
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.style.opacity = "1";
+      entry.target.style.transform = "translateY(0)";
+    }
+  });
+}, observerOptions);
 
-    document.querySelectorAll('section, article').forEach(el => {
-        el.style.opacity = "0";
-        el.style.transform = "translateY(30px)";
-        el.style.transition = "all 0.8s ease-out";
-        observer.observe(el);
-    });
+document.querySelectorAll('section, article').forEach(el => {
+  el.style.opacity = "0";
+  el.style.transform = "translateY(30px)";
+  el.style.transition = "all 0.8s ease-out";
+  observer.observe(el);
+});
 
 
 document.addEventListener('DOMContentLoaded', () => {
 
-  const horaLocal = new Intl.DateTimeFormat('es-CL?',{
+  const horaLocal = new Intl.DateTimeFormat('es-CL', {
     timeZone: 'America/Santiago',
     hour: 'numeric',
     hour12: false
@@ -51,9 +51,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const hors = parseInt(horaLocal);
   let mensaje = "";
-  if (hors >= 6 && hors < 12)      mensaje = "¡Buenos días!";
+  if (hors >= 6 && hors < 12) mensaje = "¡Buenos días!";
   else if (hors >= 12 && hors < 20) mensaje = "¡Buenas tardes!";
-  else                              mensaje = "¡Buenas noches!";
+  else mensaje = "¡Buenas noches!";
 
   const h1inicio = document.querySelector('#inicio h1');
   h1inicio.textContent = `${mensaje} Bienvenido a mi Proyecto`;
